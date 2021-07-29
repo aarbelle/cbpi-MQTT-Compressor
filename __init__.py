9843b29d570aef1d41c333905a05806f6c4365c4
aarbelle <aarbelle@gmail.com>
2021-07-29 16:30:42 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,22 @@
+try:
+  from modules.plugins.cbpi_mqtt import MQTTActor
+  
+  import time
+  @cbpi.actor
+  class MQTTCompressor(MQTTActor):
+      topic = Property.Text("Topic", configurable=True, default_value="", description="MQTT TOPIC")
+      delay = Property.Number("Delay (Min)", configurable=True, default_value=3, description="Safty delay for compressor")
+      def __init__(self):
+        super(MQTTCompressor, self).__init__()
+        self.last_off = 0 
+        self.delay_sec = float(self.delay)*60.
+      def on(self, power=100):
+          if self.last_off + self.delay_sec < time.time():
+              super(MQTTCompressor, self).on(power)
+          else:
+              print('Can not start actor for another {} seconds due to delay.'.format(self.last_off + self.delay_sec - time.time()))
+      def off(self):
+          self.last_off = time.time()
+          uper(MQTTCompressor, self).off()
+except ImportError:
+  print('Missing MQTT plugin)
