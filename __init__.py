92b6c4998111059d26fd66171c3fe64d0313bbd5
aarbelle <aarbelle@gmail.com>
2021-07-29 17:09:53 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,18 +4,21 @@ from modules.plugins.MQTTPlugin import MQTTActor
 
 import time
 #@cbpi.actor
-class MQTTCompressor(MQTTActor):
+class Compressor(object):
     topic = Property.Text("Topic", configurable=True, default_value="", description="MQTT TOPIC")
     delay = Property.Number("Delay (Min)", configurable=True, default_value=3, description="Safty delay for compressor")
     def __init__(self):
-      super(MQTTCompressor, self).__init__()
+      super(Compressor, self).__init__()
       self.last_off = 0 
       self.delay_sec = float(self.delay)*60.
     def on(self, power=100):
         if self.last_off + self.delay_sec < time.time():
-            super(MQTTCompressor, self).on(power)
+            super(Compressor, self).on(power)
         else:
             print('Can not start actor for another {} seconds due to delay.'.format(self.last_off + self.delay_sec - time.time()))
     def off(self):
         self.last_off = time.time()
-        super(MQTTCompressor, self).off()
+        super(Compressor, self).off()
+@cbpi.actor
+class MQTTCompressor(Compressor,MQTTActor):
+    pass
